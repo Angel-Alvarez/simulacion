1e62ba7a5716bbc3e57ae893df45cadf85e63c81
Angel-Alvarez <angelalvarezguzman@gmail.com>
2018-09-12 19:37:58 -0500

    se creo el proyecto

---
+++ Datos/Conexion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace Datos
+{
+    public class Conexion
+    {
+        public MySqlConnection cnConexion = new MySqlConnection();
+        public void Conectar()
+        {
+            string strCadenaConexion;
+            strCadenaConexion = "SERVER=" + "localhost" + ";PORT=3307" + ";DATABASE=" + "asesores" + ";UID=" + "root" + ";PWD=" + "root";
+            cnConexion.ConnectionString = strCadenaConexion;
+            cnConexion.Open();
+        }
+
+        public void Cerrar()
+        {
+            cnConexion.Close();
+        }
+    }
+}
---
+++ Datos/DaoFinalizar.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Pojos;
+using System.Data;
+
+namespace Datos
+{
+    class DaoFinalizar
+    {
+        Conexion conec = new Conexion();
+        public List<PojoFinalizar> obtenerFializar()
+        {
+
+            try
+            {
+
+                List<PojoFinalizar> concep = new List<PojoFinalizar>();
+                PojoFinalizar pojoFinalizar;
+                MySqlCommand cm = new MySqlCommand();
+                MySqlDataReader dr;
+                conec.Conectar();
+                string sql = " select * from asesores";
+                cm.CommandText = sql;
+                cm.CommandType = CommandType.Text;
+                cm.Connection = conec.cnConexion;
+                dr = cm.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    pojoFinalizar = new PojoFinalizar();
+                    pojoFinalizar.IdAsesores = dr.GetString("idAsesores");
+                    pojoFinalizar.NombreAsesores = dr.GetString("nombreAsesores");
+                    pojoFinalizar.ApellidoM = dr.GetString("apellidop");
+                    pojoFinalizar.ApellidoM = dr.GetString("apellidoM");
+                    pojoFinalizar.Correo = dr.GetString("correo");
+                    pojoFinalizar.Tutores_idTutores1 = dr.GetInt32("Tutores_idTutores");
+                    concep.Add(pojoFinalizar);
+                }
+                return concep;
+            }
+            catch (Exception exc)
+            {
+                return null;
+            }
+            finally
+            {
+                conec.Cerrar();
+            }
+        }
+    }
+}
