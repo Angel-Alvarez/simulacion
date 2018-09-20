218b74f193df98957ba13fd7ff99ef8f1f26dc38
Angel-Alvarez <angelalvarezguzman@gmail.com>
2018-09-19 21:33:20 -0500

    diseño de principal

--- Datos/presentacion/frmPrincipal.Designer.cs
+++ Datos/presentacion/frmPrincipal.Designer.cs
@@ -28,12 +28,240 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.BarraDeTitulo = new System.Windows.Forms.Panel();
+            this.menuVertical = new System.Windows.Forms.Panel();
+            this.panelContenedor = new System.Windows.Forms.Panel();
+            this.btnCerrar = new System.Windows.Forms.PictureBox();
+            this.btnMaximizar = new System.Windows.Forms.PictureBox();
+            this.btnMinimazar = new System.Windows.Forms.PictureBox();
+            this.btnRestaurar = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnMixtos = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnMultiplicativo = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnMedios = new System.Windows.Forms.Button();
+            this.BarraDeTitulo.SuspendLayout();
+            this.menuVertical.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMaximizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimazar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnRestaurar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BarraDeTitulo
+            // 
+            this.BarraDeTitulo.BackColor = System.Drawing.Color.Silver;
+            this.BarraDeTitulo.Controls.Add(this.btnRestaurar);
+            this.BarraDeTitulo.Controls.Add(this.btnMinimazar);
+            this.BarraDeTitulo.Controls.Add(this.btnMaximizar);
+            this.BarraDeTitulo.Controls.Add(this.btnCerrar);
+            this.BarraDeTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BarraDeTitulo.Location = new System.Drawing.Point(0, 0);
+            this.BarraDeTitulo.Name = "BarraDeTitulo";
+            this.BarraDeTitulo.Size = new System.Drawing.Size(1300, 38);
+            this.BarraDeTitulo.TabIndex = 0;
+            this.BarraDeTitulo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BarraDeTitulo_MouseDown);
+            // 
+            // menuVertical
+            // 
+            this.menuVertical.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(110)))), ((int)(((byte)(200)))));
+            this.menuVertical.Controls.Add(this.panel3);
+            this.menuVertical.Controls.Add(this.btnMedios);
+            this.menuVertical.Controls.Add(this.panel2);
+            this.menuVertical.Controls.Add(this.btnMultiplicativo);
+            this.menuVertical.Controls.Add(this.panel1);
+            this.menuVertical.Controls.Add(this.btnMixtos);
+            this.menuVertical.Controls.Add(this.pictureBox1);
+            this.menuVertical.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuVertical.Location = new System.Drawing.Point(0, 38);
+            this.menuVertical.Name = "menuVertical";
+            this.menuVertical.Size = new System.Drawing.Size(220, 612);
+            this.menuVertical.TabIndex = 1;
+            this.menuVertical.Paint += new System.Windows.Forms.PaintEventHandler(this.menuVertical_Paint);
+            // 
+            // panelContenedor
+            // 
+            this.panelContenedor.BackColor = System.Drawing.Color.White;
+            this.panelContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContenedor.Location = new System.Drawing.Point(220, 38);
+            this.panelContenedor.Name = "panelContenedor";
+            this.panelContenedor.Size = new System.Drawing.Size(1080, 612);
+            this.panelContenedor.TabIndex = 2;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrar.Image")));
+            this.btnCerrar.Location = new System.Drawing.Point(1272, 7);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(25, 25);
+            this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnCerrar.TabIndex = 0;
+            this.btnCerrar.TabStop = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // btnMaximizar
+            // 
+            this.btnMaximizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMaximizar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMaximizar.Image = ((System.Drawing.Image)(resources.GetObject("btnMaximizar.Image")));
+            this.btnMaximizar.Location = new System.Drawing.Point(1241, 7);
+            this.btnMaximizar.Name = "btnMaximizar";
+            this.btnMaximizar.Size = new System.Drawing.Size(25, 25);
+            this.btnMaximizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnMaximizar.TabIndex = 1;
+            this.btnMaximizar.TabStop = false;
+            this.btnMaximizar.Click += new System.EventHandler(this.btnMaximizar_Click);
+            // 
+            // btnMinimazar
+            // 
+            this.btnMinimazar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimazar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMinimazar.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimazar.Image")));
+            this.btnMinimazar.Location = new System.Drawing.Point(1210, 7);
+            this.btnMinimazar.Name = "btnMinimazar";
+            this.btnMinimazar.Size = new System.Drawing.Size(25, 25);
+            this.btnMinimazar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnMinimazar.TabIndex = 2;
+            this.btnMinimazar.TabStop = false;
+            this.btnMinimazar.Click += new System.EventHandler(this.btnMinimazar_Click);
+            // 
+            // btnRestaurar
+            // 
+            this.btnRestaurar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRestaurar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRestaurar.Image = ((System.Drawing.Image)(resources.GetObject("btnRestaurar.Image")));
+            this.btnRestaurar.Location = new System.Drawing.Point(1241, 7);
+            this.btnRestaurar.Name = "btnRestaurar";
+            this.btnRestaurar.Size = new System.Drawing.Size(25, 25);
+            this.btnRestaurar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnRestaurar.TabIndex = 3;
+            this.btnRestaurar.TabStop = false;
+            this.btnRestaurar.Visible = false;
+            this.btnRestaurar.Click += new System.EventHandler(this.btnRestaurar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(5, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(217, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnMixtos
+            // 
+            this.btnMixtos.FlatAppearance.BorderSize = 0;
+            this.btnMixtos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnMixtos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMixtos.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMixtos.ForeColor = System.Drawing.Color.White;
+            this.btnMixtos.Location = new System.Drawing.Point(5, 112);
+            this.btnMixtos.Name = "btnMixtos";
+            this.btnMixtos.Size = new System.Drawing.Size(212, 36);
+            this.btnMixtos.TabIndex = 1;
+            this.btnMixtos.Text = "Mixtos";
+            this.btnMixtos.UseVisualStyleBackColor = true;
+            this.btnMixtos.Click += new System.EventHandler(this.btnMixtos_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Location = new System.Drawing.Point(5, 112);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(5, 36);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Silver;
+            this.panel2.Location = new System.Drawing.Point(5, 154);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(5, 36);
+            this.panel2.TabIndex = 4;
+            // 
+            // btnMultiplicativo
+            // 
+            this.btnMultiplicativo.FlatAppearance.BorderSize = 0;
+            this.btnMultiplicativo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnMultiplicativo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMultiplicativo.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicativo.ForeColor = System.Drawing.Color.White;
+            this.btnMultiplicativo.Location = new System.Drawing.Point(5, 154);
+            this.btnMultiplicativo.Name = "btnMultiplicativo";
+            this.btnMultiplicativo.Size = new System.Drawing.Size(212, 36);
+            this.btnMultiplicativo.TabIndex = 3;
+            this.btnMultiplicativo.Text = " Multiplicativo";
+            this.btnMultiplicativo.UseVisualStyleBackColor = true;
+            this.btnMultiplicativo.Click += new System.EventHandler(this.btnMultiplicativo_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Silver;
+            this.panel3.Location = new System.Drawing.Point(5, 196);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(5, 36);
+            this.panel3.TabIndex = 6;
+            // 
+            // btnMedios
+            // 
+            this.btnMedios.FlatAppearance.BorderSize = 0;
+            this.btnMedios.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnMedios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMedios.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMedios.ForeColor = System.Drawing.Color.White;
+            this.btnMedios.Location = new System.Drawing.Point(3, 196);
+            this.btnMedios.Name = "btnMedios";
+            this.btnMedios.Size = new System.Drawing.Size(212, 36);
+            this.btnMedios.TabIndex = 5;
+            this.btnMedios.Text = "Cuadrados medios";
+            this.btnMedios.UseVisualStyleBackColor = true;
+            this.btnMedios.Click += new System.EventHandler(this.btnMedios_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1300, 650);
+            this.Controls.Add(this.panelContenedor);
+            this.Controls.Add(this.menuVertical);
+            this.Controls.Add(this.BarraDeTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmPrincipal";
             this.Text = "Form1";
+            this.BarraDeTitulo.ResumeLayout(false);
+            this.menuVertical.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMaximizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimazar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnRestaurar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel BarraDeTitulo;
+        private System.Windows.Forms.PictureBox btnCerrar;
+        private System.Windows.Forms.Panel menuVertical;
+        private System.Windows.Forms.Panel panelContenedor;
+        private System.Windows.Forms.PictureBox btnRestaurar;
+        private System.Windows.Forms.PictureBox btnMinimazar;
+        private System.Windows.Forms.PictureBox btnMaximizar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnMixtos;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnMedios;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnMultiplicativo;
     }
 }
 
--- Datos/presentacion/frmPrincipal.cs
+++ Datos/presentacion/frmPrincipal.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace presentacion
 {
@@ -15,6 +16,81 @@ namespace presentacion
         public frmPrincipal()
         {
             InitializeComponent();
+        }
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMaximizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            btnMaximizar.Visible = false;
+            btnRestaurar.Visible = true;
+
+
+        }
+
+        private void btnRestaurar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            btnMaximizar.Visible = true;
+            btnRestaurar.Visible = false;
+        }
+
+        private void btnMinimazar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnMixtos_Click(object sender, EventArgs e)
+        {
+            frmMixto m = new frmMixto();
+            AbrirForm(m);
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void menuVertical_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void BarraDeTitulo_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+
+        }
+        private void AbrirForm(object formHijo)
+        {
+            Form fh = formHijo as Form;
+            fh.TopLevel = false;
+            this.panelContenedor.Controls.Add(fh);
+            this.panelContenedor.Tag = fh;
+            fh.Show();
+
+        }
+
+        private void btnMultiplicativo_Click(object sender, EventArgs e)
+        {
+            frmMultiplicativo mul = new frmMultiplicativo();
+            AbrirForm(mul);
+        }
+
+        private void btnMedios_Click(object sender, EventArgs e)
+        {
+            frmCuadradosMedios cua = new frmCuadradosMedios();
+            AbrirForm(cua);
         }
     }
 }
